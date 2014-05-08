0d9e5e80d9673705c28f7c6bbdd6de3b29dd5001
liaojinxing <jinxingliao@gmail.com>
2014-05-08 18:59:32 +0800

    Initial Commit

---
+++ ButtonIndicatorView/ButtonIndicatorView.h
@@ -0,0 +1,13 @@
+//
+//  ButtonIndicatorView.h
+//  ButtonIndicatorView
+//
+//  Created by liaojinxing on 14-5-8.
+//  Copyright (c) 2014年 jinxing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ButtonIndicatorView : NSObject
+
+@end
---
+++ ButtonIndicatorView/ButtonIndicatorView.m
@@ -0,0 +1,13 @@
+//
+//  ButtonIndicatorView.m
+//  ButtonIndicatorView
+//
+//  Created by liaojinxing on 14-5-8.
+//  Copyright (c) 2014年 jinxing. All rights reserved.
+//
+
+#import "ButtonIndicatorView.h"
+
+@implementation ButtonIndicatorView
+
+@end
